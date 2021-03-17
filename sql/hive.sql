868f9d0a9f1615f50976bd08d49b64a035908450
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-03-17 10:48:24 -0400

    Update hive.sql

--- sql/hive.sql
+++ sql/hive.sql
@@ -23,3 +23,42 @@ CREATE TABLE hv_stocks
   `volume` STRING,
   `low` STRING,
    PRIMARY KEY (UUID) DISABLE NOVALIDATE);
+
+ CREATE EXTERNAL TABLE scada2 
+(
+uuid STRING, 
+	systemtime STRING,  
+	amplitude100 DOUBLE, 
+        amplitude500 DOUBLE, 
+	amplitude1000 DOUBLE, 
+	lownoise DOUBLE, 
+	midnoise DOUBLE,
+        highnoise DOUBLE, 
+	amps DOUBLE, 
+	ipaddress STRING, 
+	host STRING, 
+	host_name STRING,
+        macaddress STRING, 
+	endtime STRING, 
+	runtime STRING, 
+	starttime STRING, 
+        cpu DOUBLE, 
+	cpu_temp STRING, 
+	diskusage STRING, 
+	memory DOUBLE, 
+	id STRING, 
+	temperature STRING, 
+	adjtemp STRING, 
+	adjtempf STRING, 
+	temperaturef STRING, 
+	pressure DOUBLE, 
+	humidity DOUBLE, 
+	lux DOUBLE, 
+	proximity INT, 
+	oxidising DOUBLE, 
+	reducing DOUBLE, 
+	nh3 DOUBLE, 
+	gasko STRING)
+  ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
+  LOCATION '/tmp/demo';
+  
