6d641966e4bf77d3875babc37be68f3158bec81d
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-04-14 17:29:41 -0400

    Update ssb.sql

--- flinksql/ssb.sql
+++ flinksql/ssb.sql
@@ -8,6 +8,17 @@ CAST(`close` as DOUBLE) >
 (SELECT MAX(CAST(`close` as DOUBLE)) FROM stocksraw s 
 WHERE s.symbol = st.symbol)
             
+SELECT HOP_END(eventTimestamp, INTERVAL '1' SECOND, INTERVAL '30' SECOND) as windowEnd,
+            count(`close`) as closeCount, 
+            sum(cast(`close` as float)) as closeSum, 
+            avg(cast(`close` as float)) as closeAverage, 
+            min(`close`) as closeMin, 
+            max(`close`) as closeMax, 
+            sum(case when `close` > 14 then 1 else 0 end) as stockGreaterThan14 
+FROM stocksraw WHERE symbol = 'CLDR' 
+GROUP BY HOP(eventTimestamp, INTERVAL '1' SECOND, INTERVAL '30' SECOND)
+
+
             
 
 
