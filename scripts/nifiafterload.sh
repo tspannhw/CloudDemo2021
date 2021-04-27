5a5afd28743c136c7086c7b20aec5d722143f7f0
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-04-27 12:23:23 -0400

    Update nifiafterload.sh

--- scripts/nifiafterload.sh
+++ scripts/nifiafterload.sh
@@ -13,11 +13,11 @@
 # need to get pg id for stocks 2 and weather from list
 # need to get pc from list
 
-/opt/demo/nifi-toolkit-1.13.2/bin/cli.sh nifi pg-set-param-context -u http://edge2ai-1.dim.local:8080 -verbose -pgid 6add2d4c-28d0-36e6-b2ea-551affdcf6de -pcid cfc79628-0177-1000-0000-0000034fb27a
+# /opt/demo/nifi-toolkit-1.13.2/bin/cli.sh nifi pg-set-param-context -u http://edge2ai-1.dim.local:8080 -verbose -pgid 6add2d4c-28d0-36e6-b2ea-551affdcf6de -pcid cfc79628-0177-1000-0000-0000034fb27a
 
-/opt/demo/nifi-toolkit-1.13.2/bin/cli.sh nifi pg-set-param-context -u http://edge2ai-1.dim.local:8080 -verbose -pgid 33a15479-6ab4-35a7-bde9-0fd19d780e59 -pcid cfc79628-0177-1000-0000-0000034fb27a
+# /opt/demo/nifi-toolkit-1.13.2/bin/cli.sh nifi pg-set-param-context -u http://edge2ai-1.dim.local:8080 -verbose -pgid 33a15479-6ab4-35a7-bde9-0fd19d780e59 -pcid cfc79628-0177-1000-0000-0000034fb27a
 
-/opt/demo/nifi-toolkit-1.13.2/bin/cli.sh nifi pg-set-param-context -u http://edge2ai-1.dim.local:8080 -verbose -pgid 2a4e99f2-56e7-34df-85e1-b56b0f918dac  -pcid cfc79628-0177-1000-0000-0000034fb27a
+# /opt/demo/nifi-toolkit-1.13.2/bin/cli.sh nifi pg-set-param-context -u http://edge2ai-1.dim.local:8080 -verbose -pgid 2a4e99f2-56e7-34df-85e1-b56b0f918dac  -pcid cfc79628-0177-1000-0000-0000034fb27a
 
 /opt/demo/nifi-toolkit-1.13.2/bin/cli.sh nifi pg-enable-services --processGroupId root -u http://edge2ai-1.dim.local:8080 
 
