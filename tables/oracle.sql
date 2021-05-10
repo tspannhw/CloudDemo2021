72f744f87f5fa83439bb52a596d8f335717a92bd
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-05-10 16:46:19 -0400

    Create oracle.sql

---
+++ tables/oracle.sql
@@ -0,0 +1,20 @@
+CREATE TABLE VACCINE
+(
+    vaccine_id number(10) NOT NULL,
+    vaccine_name varchar2(50) NOT NULL,
+    vaccine_dose number(10),
+    vaccine_age varchar2(50),
+    CONSTRAINT vaccine_pk PRIMARY KEY (vaccine_id)
+);
+
+CREATE TABLE cdc.transcomevents ( 
+uuid varchar2(64), 
+ts TIMESTAMP,  
+title varchar2(255), 
+description varchar2(255), 
+pubDate varchar2(255), 
+point varchar2(255),
+	 latitude FLOAT(126), 
+   longitude FLOAT(126), 
+    CONSTRAINT uuid_pk PRIMARY KEY (uuid)
+);
