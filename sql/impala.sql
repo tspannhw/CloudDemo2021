08578f7c2e4085be5c70dd1eb1dda503aca36059
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-05-18 14:42:32 -0400

    Update impala.sql

--- sql/impala.sql
+++ sql/impala.sql
@@ -16,3 +16,23 @@ LOCATION '/tmp/stocks/stocks3';
 
 
 # Kafka Connect to parquet requires a schema without union types so no Nulls and no timestamps
+
+
+CREATE EXTERNAL TABLE scada
+(
+uuid STRING, 
+	systemtime STRING,  
+temperaturef STRING,
+    pressure double,
+    humidity double,
+    lux double,
+    proximity int,
+    oxidising double,
+    reducing double,
+    nh3 double, gasko string, amplitude100 double, amplitude500 double, amplitude1000 double, lownoise double, midnoise double, highnoise double,
+    amps double, cpu double, memory double, ipaddress string, host string, host_name string, macaddress string, endtime string, runtime string, starttime string,
+    cpu_temp string, diskusage string, id string, temperature string, adjtemp string, adjtempf string,
+PRIMARY KEY (uuid) ) 
+STORED AS PARQUET
+LOCATION '/tmp/sensors2';
+
